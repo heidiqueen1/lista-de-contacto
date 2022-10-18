0ee4ea9927081fafe53e605a6d614c180879e900
Heidi Sánchez <heidisanchez103.9@gmail.com>
2022-10-17 20:59:19 -0500

    mi lista de contacto

---
+++ contactlist.js
@@ -0,0 +1,11 @@
+
+const contactList = ["HeidiSanchez","PedroSanchez","JuanVolpe","CamiloPerez"];
+const newContactList = contactList.push("LuisaRojas");
+console.log(contactList);
+console.log(newContactList);
+const newContactListDos = contactList.pop();
+console.log (contactList);
+
+
+
+
