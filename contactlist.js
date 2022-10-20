41c277b357d7d8f71afc38feb399f926ce3c1651
Heidi Sánchez <heidisanchez103.9@gmail.com>
2022-10-20 17:13:39 -0500

    cree una lista de contacto como objeto y dos funciones para agregar y eliminar contactos

--- contactlist.js
+++ contactlist.js
@@ -1,11 +1,71 @@
+function addContact() {
+    const contact = {
+      id: parseInt(prompt("Ingrese un número de id con tres dígitos")),
+      name: prompt("Ingrese el nombre del contacto"),
+      surname: prompt("Ingrese el apellido del contacto"),
+      cellphone: parseInt(prompt("Ingrese un número de teléfono")),
+      location: [prompt("Ingrese Ciudad"), prompt("Ingrese país")],
+    };
+  
+     contactList.push(contact);
+   
+  }
 
-const contactList = ["HeidiSanchez","PedroSanchez","JuanVolpe","CamiloPerez"];
-const newContactList = contactList.push("LuisaRojas");
+
+ 
+  function eraseContact() {
+  const position = parseInt(
+    prompt(" Introducir el número de la posición del contacto que desea borrar")
+  );
+  contactList.splice(position, 1);
+};
+
+
+
+const contactList = [
+  {
+    id: 001,
+    name: "Heidi",
+    surname: "Sanchez",
+    cellphone: 5412962,
+    location: [
+      {
+        city: "Conocoto",
+        adress: " Barrio Pichincha",
+      },
+    ],
+  },
+  {
+    id: 002,
+    name: "Pedro",
+    surname: "Sanchez",
+    cellphone: 5410000,
+    location: [
+      {
+        city: "Cartagena",
+        adress: " Barrio La Costanera",
+      },
+    ],
+  },
+  {
+    id: 003,
+    name: "Juan",
+    surname: "Volpe",
+    cellphone: 5412200,
+    location: [
+      {
+        city: "Buenos Aires",
+        adress: "Barrio La Perla",
+      },
+    ],
+  },
+];
+
+
+
+
+
+addContact();
+eraseContact();
 console.log(contactList);
-console.log(newContactList);
-const newContactListDos = contactList.pop();
-console.log (contactList);
-
-
-
 
